77d9b85ecfe0de6673a1e2cfabe5234796b550a0
Iason Krommydas <iason.krom@gmail.com>
2023-01-11 17:09:15 -0600

    added DY CR

---
+++ ControlRegion/dyttcr/cuts.py
@@ -0,0 +1,54 @@
+# cuts
+
+supercut = ' mll > 12 \
+            && Lepton_pt[0]>25 \
+            && Lepton_pt[1]>13 \
+            && (nLepton>=2 && Alt$(Lepton_pt[2],0)<10) \
+            && abs(Lepton_eta[0])<2.5 && abs(Lepton_eta[1])<2.5 \
+            && ptll>30 \
+            && PuppiMET_pt > 20 \
+            && (Lepton_pdgId[0]*Lepton_pdgId[1] == -11*13) \
+           '
+
+# ## Signal regions
+# cuts['hww2l2v_13TeV'] = {
+#    'expr': 'sr',
+#     # Define the sub-categorization of sr
+#    'categories' : {
+#       '0j' : 'zeroJet',
+#       #
+#       '1j' : 'oneJet && Alt$(CleanJet_pt[1],0)<30',
+#       #
+#       #'em_pm_0j_pt2ge20' : ' Lepton_pdgId[0]==-11 && Lepton_pt[1]>=20 && zeroJet',
+#       #'em_mp_0j_pt2ge20' : ' Lepton_pdgId[0]==11 && Lepton_pt[1]>=20 && zeroJet',
+#       #'me_mp_0j_pt2ge20' : ' Lepton_pdgId[0]==-13 && Lepton_pt[1]>=20 && zeroJet',
+#       #'me_pm_0j_pt2ge20' : ' Lepton_pdgId[0]==13 && Lepton_pt[1]>=20 && zeroJet',
+#       ##
+#       #'em_pm_0j_pt2lt20' : ' Lepton_pdgId[0]==-11 && Lepton_pt[1]<20 && zeroJet',
+#       #'em_mp_0j_pt2lt20' : ' Lepton_pdgId[0]==11 && Lepton_pt[1]<20 && zeroJet',
+#       #'me_mp_0j_pt2lt20' : ' Lepton_pdgId[0]==-13 && Lepton_pt[1]<20 && zeroJet',
+#       #'me_pm_0j_pt2lt20' : ' Lepton_pdgId[0]==13 && Lepton_pt[1]<20 && zeroJet',
+#       ##
+#       #'em_pm_1j_pt2ge20' : ' Lepton_pdgId[0]==-11 && Lepton_pt[1]>=20 && oneJet && Alt$(CleanJet_pt[1],0)<30',
+#       #'em_mp_1j_pt2ge20' : ' Lepton_pdgId[0]==11 && Lepton_pt[1]>=20 && oneJet && Alt$(CleanJet_pt[1],0)<30',
+#       #'me_mp_1j_pt2ge20' : ' Lepton_pdgId[0]==-13 && Lepton_pt[1]>=20 && oneJet && Alt$(CleanJet_pt[1],0)<30',
+#       #'me_pm_1j_pt2ge20' : ' Lepton_pdgId[0]==13 && Lepton_pt[1]>=20 && oneJet && Alt$(CleanJet_pt[1],0)<30',
+#       ##
+#       #'em_pm_1j_pt2lt20' : ' Lepton_pdgId[0]==-11 && Lepton_pt[1]<20 && oneJet && Alt$(CleanJet_pt[1],0)<30',
+#       #'em_mp_1j_pt2lt20' : ' Lepton_pdgId[0]==11 && Lepton_pt[1]<20 && oneJet && Alt$(CleanJet_pt[1],0)<30',
+#       #'me_mp_1j_pt2lt20' : ' Lepton_pdgId[0]==-13 && Lepton_pt[1]<20 && oneJet && Alt$(CleanJet_pt[1],0)<30',
+#       #'me_pm_1j_pt2lt20' : ' Lepton_pdgId[0]==13 && Lepton_pt[1]<20 && oneJet && Alt$(CleanJet_pt[1],0)<30',
+#       #
+#       '2j' : 'multiJet', 
+#    }
+# }
+
+## DYtt control regions
+cuts['hww2l2v_13TeV_dy']  = { 
+   'expr' : 'dycr',
+   'categories' : {
+      '0j' : 'zeroJet',
+      '1j' : 'oneJet && Alt$(CleanJet_pt[1],0)<30',
+      '2j' : 'multiJet', 
+   }
+}
